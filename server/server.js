7b87c512ea5058dde14d3ea7809dcd83411b2a62
Jong Hwan Seo <jseo51@my.bcit.ca>
2022-09-09 14:09:20 -0700

    Changed api key to my own and changed GET and POST requests

--- server/server.js
+++ server/server.js
@@ -6,20 +6,24 @@ app.listen(8080, function(err) {
   if(err) console.log(err);
 })
 
-var apikey = "b660f3402c54cb9a9c48f89c35249e5c";
+var apikey = "efd172570402c278b06459e2135c88c6";
 
-app.get("/", function(req, res) {
-  var cityName = 'Vancouver';
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=metric&appid=" + apikey;
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+})
+
+app.post("/", function(req, res) {
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + req.body.cityName + "&units=metric&appid=" + apikey
 
   https.get(url, function(https_res) {
     https_res.on("data", function(data) {
-      res.write("<h1> " + cityName + " weather is " + JSON.parse(data).weather[0].description) + "</h1>";
-      res.write("<h1> " + cityName + " temp is " + JSON.parse(data).main.temp) + "</h1>";
+      res.write("<h1> " + req.body.cityName + " weather is " + JSON.parse(data).weather[0].description) + "</h1>";
+      res.write("<h1> " + req.body.cityName + " temp is " + JSON.parse(data).main.temp) + "</h1>";
       res.write('  <img src="' + "http://openweathermap.org/img/wn/" + JSON.parse(data).weather[0].icon + '.png"' + "/>");
       res.send();
     })
-  })
+  });
+
 })
 
 app.get('/contact', function (req, res) {
