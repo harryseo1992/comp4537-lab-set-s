86581ef39d0e66acccb74a780d0b32678366898a
Jong Hwan Seo <jseo51@my.bcit.ca>
2022-09-09 14:03:35 -0700

    Changed the prev GET request to get Vancouver temp

--- server/server.js
+++ server/server.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const app = express();
+const https = require('https');
 
 app.listen(8080, function(err) {
   if(err) console.log(err);
 })
 
-app.get('/', function (req, res) {
-  res.send('GET request to homepage')
+var apikey = "b660f3402c54cb9a9c48f89c35249e5c";
+
+app.get("/", function(req, res) {
+  var cityName = 'Vancouver';
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=metric&appid=" + apikey;
+
+  https.get(url, function(https_res) {
+    https_res.on("data", function(data) {
+      res.write("<h1> " + cityName + " weather is " + JSON.parse(data).weather[0].description + "</h1>");
+      res.write("<h1> " + cityName + " temp is " + JSON.parse(data).main.temp + "</h1>");
+      res.write('  <img src="' + "http://openweathermap.org/img/wn/" + JSON.parse(data).weather[0].icon + '.png"' + "/>");
+      res.send();
+    })
+  })
 })
 
 app.get('/contact', function (req, res) {
