f84f22e557d2f49a4b3d70f81614d36e1a629186
Jong Hwan Seo <jseo51@my.bcit.ca>
2022-09-11 15:54:29 -0700

    Removed unused and unfound import

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Header from './Header'
 import Cities from './Cities';
-import './index.css'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
