6cccd078d7241adfd6c94d015fb69e2e989b4a65
Jong Hwan Seo <jseo51@my.bcit.ca>
2022-09-29 15:34:41 -0700

    created unicorns db via mongodb

---
+++ lab4/server.js
@@ -0,0 +1,6 @@
+// app.get('/api/v2/unicorns')           // - get all the unicorns
+// app.post('/api/v2/unicorn')          // - create a new unicorn
+// app.get('/api/v2/unicorn/:id')       // - get a unicorn
+// app.patch('/api/v2/unicorn/:id')     // - update a unicorn
+// app.delete('/api/v2/unicorn/:id')       // - delete unicorns
+
