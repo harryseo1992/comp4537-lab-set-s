158b0116c2586f2eb49e6561f60cad609cc858b9
Jong Hwan Seo <jseo51@my.bcit.ca>
2022-10-21 10:32:22 -0700

    Attempting to set up proxy

--- midterm/server.js
+++ midterm/server.js
@@ -1,12 +1,25 @@
 const https = require('https');
 const mongoose = require('mongoose');
 const express = require('express');
-const url = "https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/pokedex.json";
-
+const morgan = require('morgan');
+const url = process.env.API_BASE_URL;
+const { createProxyMiddleware } = require('http-proxy-middleware');
+require('dotenv').config();
 const { Schema } = mongoose;
 
 var pokeModel = null;
 
 const app = express();
 const port = 8899;
+app.use('/pokedex', createProxyMiddleware({
+  target: API_BASE_URL,
+  changeOrigin: true,
+  pathRewrite: {
+    "^/pokedex": '',
+  },
+}));
 
+// Start proxy
+app.listen(port, () => {
+  console.log(`Starting proxy at ${port}`);
+})
