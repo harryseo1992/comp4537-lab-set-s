73605ae4c1ce2323f4b6ca56c479512bffafad5f
Jong Hwan Seo <jseo51@my.bcit.ca>
2022-10-21 10:43:44 -0700

    Changed server.js so that it starts out with the schema but doesn't populate the local db

--- midterm/server.js
+++ midterm/server.js
@@ -11,15 +11,52 @@ var pokeModel = null;
 
 const app = express();
 const port = 8899;
-app.use('/pokedex', createProxyMiddleware({
-  target: API_BASE_URL,
-  changeOrigin: true,
-  pathRewrite: {
-    "^/pokedex": '',
-  },
-}));
 
-// Start proxy
-app.listen(port, () => {
-  console.log(`Starting proxy at ${port}`);
+app.listen(port, async () => {
+  try {
+    const x = await mongoose.connect('mongodb://localhost:27017/test')
+    mongoose.connection.db.dropDatabase();
+  } catch (error) {
+    console.log ('db error');
+  }
+
+  var possibleTypes = [];
+  var pokeSchema = null;
+
+  await https.get("https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/types.json", async (res) => {
+    var chunks = "";
+    res.on("data", function (chunk) {
+      chunks += chunk;
+    });
+    await res.on("end", async (data) => {
+      possibleTypes = JSON.parse(chunks)
+      possibleTypes = await possibleTypes.map(element => element.english);
+      pokeSchema = new Schema({
+        "id": {
+          type: Number,
+          unique: [true, "You cannot have two pokemons with the same id"]
+        },
+        "name": {
+          "english": {
+            type: String,
+            required: true,
+            maxlength: [20, "Name should be less than 20 characters long"]
+          },
+          "japanese": String,
+          "chinese": String,
+          "french": String
+        },
+        "type": possibleTypes,
+        "base": {
+          "HP": Number,
+          "Attack": Number,
+          "Defense": Number,
+          "Speed Attack": Number,
+          "Speed Defense": Number,
+          "Speed": Number
+        }
+      })
+      pokeModel = mongoose.model('pokemons', pokeSchema);
+    });
+  })
 })
