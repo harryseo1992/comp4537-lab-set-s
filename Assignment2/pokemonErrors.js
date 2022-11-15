64b94e0d36b5f15e1ee4da6a575e7b112af282ad
Jong Hwan Seo <jseo51@my.bcit.ca>
2022-11-15 10:52:14 -0800

    Added additional fields inside pokemon errors - pokeErrCode and default messages

--- Assignment2/pokemonErrors.js
+++ Assignment2/pokemonErrors.js
@@ -1,7 +1,9 @@
 class PokemonBadRequest extends Error {
   constructor(message) {
     super(message);
-    this.name = "PokemonBadRequest";
+    this.name = 'PokemonBadRequest';
+    this.message = "Error - Bad request: check the API doc";
+    this.pokeErrCode = 400;
   }
 }
 
@@ -9,6 +11,8 @@ class PokemonBadRequestMissingID extends PokemonBadRequest {
   constructor(message) {
     super(message);
     this.name = "PokemonBadRequestMissingID";
+    this.message = "Error - Bad request - missing ID: check the API doc";
+    this.pokeErrCode = 400;
   }
 }
 
@@ -16,6 +20,8 @@ class PokemonBadRequestMissingCount extends PokemonBadRequest {
   constructor(message) {
     super(message);
     this.name = "PokemonBadRequestMissingCount";
+    this.message = "Error - Bad request - missing count: check the API doc";
+    this.pokeErrCode = 400;
   }
 }
 
@@ -23,6 +29,8 @@ class PokemonBadRequestImproperCount extends PokemonBadRequest {
   constructor(message) {
     super(message);
     this.name = "PokemonBadRequestImproperCount";
+    this.message = "Error - Bad request - improper count value: check the API doc";
+    this.pokeErrCode = 400;
   }
 }
 
@@ -30,13 +38,26 @@ class PokemonBadRequestMissingAfter extends PokemonBadRequest {
   constructor(message) {
     super(message);
     this.name = "PokemonBadRequestMissingAfter";
+    this.message = "Error - Bad request - missing after: check the API doc";
+    this.pokeErrCode = 400;
+  }
+}
+
+class PokemonDuplicateError extends PokemonBadRequest {
+  constructor(message) {
+    super(message);
+    this.name = 'PokemonDuplicateError';
+    this.message = "Error - PokemonDuplicateError: The Pokemons has already been inserted.";
+    this.pokeErrCode = 400;
   }
 }
 
 class PokemonDbError extends Error {
   constructor(message) {
     super(message);
-    this.name = "PokemonDbError";
+    this.name = 'PokemonDbError';
+    this.message = "Error - DB error: Contact API owners for more info.";
+    this.pokeErrCode = 500;
   }
 }
 
@@ -44,6 +65,8 @@ class PokemonNotFoundError extends PokemonBadRequest {
   constructor(message) {
     super(message);
     this.name = "PokemonNotFoundError";
+    this.message = "Error - Bad request - pokemon not found: check the API doc";
+    this.pokeErrCode = 400;
   }
 }
 
@@ -51,6 +74,17 @@ class PokemonImageNotFoundError extends PokemonDbError {
   constructor(message) {
     super(message);
     this.name = "PokemonImageNotFoundError";
+    this.message = "Error - DB error: Contact API owners for more info.";
+    this.pokeErrCode = 500;
+  }
+}
+
+class PokemonNoSuchRouteError extends PokemonBadRequest {
+  constructor(message) {
+    super(message);
+    this.name = 'PokemonNoSuchRouteError';
+    this.message = "Error - Improper Route: check the API doc";
+    this.pokeErrCode = 404;
   }
 }
 
@@ -62,5 +96,7 @@ module.exports = {
   PokemonBadRequestMissingAfter,
   PokemonDbError,
   PokemonNotFoundError,
-  PokemonImageNotFoundError
+  PokemonImageNotFoundError,
+  PokemonDuplicateError,
+  PokemonNoSuchRouteError
 }
