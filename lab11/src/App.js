59e335f636d661d5ddb04f1f27d7c8f1291d952a
Jong Hwan Seo <jseo51@my.bcit.ca>
2022-11-22 01:31:50 -0800

    Took out unnecessary box and grids

--- lab11/src/App.js
+++ lab11/src/App.js
@@ -1,16 +1,10 @@
 import React from "react";
-import { Box } from "@mui/system";
-import { Grid } from "@mui/material";
 import FilterablePokemon from "./Components/FilterablePokemon/FilterablePokemon.js";
 
 const App = () => {
   return (
     <>
-      <Box>
-        <Grid container>
-          <FilterablePokemon />
-        </Grid>
-      </Box>
+      <FilterablePokemon />
     </>
   );
 };
