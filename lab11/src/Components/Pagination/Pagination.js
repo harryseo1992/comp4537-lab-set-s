d1af9f1a5062b89a92c3563ef4fd88fea161c9ce
Jong Hwan Seo <jseo51@my.bcit.ca>
2022-11-21 20:28:43 -0800

    Set up page and pagination for lab 11

---
+++ lab11/src/Components/Pagination/Pagination.js
@@ -0,0 +1,68 @@
+import React from "react";
+import "./Pagination.css";
+const Pagination = ({
+  numberOfPages,
+  currentPage,
+  setCurrentPage,
+  pageLimit,
+}) => {
+  const pageNumbers = [];
+  for (let i = 1; i <= numberOfPages; i++) {
+    pageNumbers.push(i);
+  }
+
+  const goToNextPage = () => {
+    if (currentPage != numberOfPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const goToPreviousPage = () => {
+    if (currentPage !== 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  function changePage(event) {
+    const pageNumber = Number(event.target.textContent);
+    setCurrentPage(pageNumber);
+  }
+
+  const getPaginationGroup = () => {
+    let start = Math.floor((currentPage - 1) / pageLimit) * pageLimit;
+    return new Array(pageLimit).fill().map((_, idx) => start + idx + 1);
+  };
+
+  return (
+    <div className="pagination">
+      {/* previous button */}
+      <button
+        onClick={goToPreviousPage}
+        className={`prev ${currentPage === 1 ? "disabled" : ""}`}
+      >
+        prev
+      </button>
+
+      {/* show page numbers */}
+      {getPaginationGroup().map((item, index) => (
+        <button
+          key={index}
+          onClick={changePage}
+          className={`paginationItem ${currentPage === item ? "active" : null}`}
+        >
+          <span>{item}</span>
+        </button>
+      ))}
+
+      {/* next button */}
+      <button
+        onClick={goToNextPage}
+        className={`next ${currentPage === numberOfPages ? "disabled" : ""}`}
+      >
+        next
+      </button>
+    </div>
+  );
+};
+
+export default Pagination;
