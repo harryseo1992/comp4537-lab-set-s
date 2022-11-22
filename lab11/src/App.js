80603f3891b0670e0dfbc49f68758939123488ec
Jong Hwan Seo <jseo51@my.bcit.ca>
2022-11-22 00:14:57 -0800

    Changed structure so that App only returns FilterablePokemon

--- lab11/src/App.js
+++ lab11/src/App.js
@@ -1,67 +1,16 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 import { Box } from "@mui/system";
-import Page from "./Components/Page/Page.js";
-import Pagination from "./Components/Pagination/Pagination.js";
 import { Grid } from "@mui/material";
+import FilterablePokemon from "./Components/FilterablePokemon/FilterablePokemon.js";
 
 const App = () => {
-  const [pokemons, setPokemons] = useState([]);
-  const [searchQueries, setSearchQueries] = useState({
-    searchByName: "",
-    searchByType: "",
-    searchByHp: "0",
-    searchByAttack: "0",
-    searchByDefense: "0",
-    searchBySpAttack: "0",
-    searchBySpDefense: "0",
-    searchBySpeed: "0",
-  });
-  const [filteredPokemons, setFilteredPokemons] = useState(pokemons);
-  const handleChange = (e) => {
-    setSearchByName(e.target.value);
-  };
-  useEffect(() => {
-    const fetchPokemons = () => {
-      axios
-        .get(
-          "https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/pokedex.json"
-        )
-        .then((res) => res.data)
-        .then((data) => setPokemons(data));
-    };
-    fetchPokemons();
-    setFilteredPokemons(pokemons.filter());
-  }, []);
-
-  const indexOfLastPokemon = currentPage * pokemonsPerPage;
-  const indexOfFirstPokemon = indexOfLastPokemon - pokemonsPerPage;
-
-  const currentPokemons = pokemons.slice(
-    indexOfFirstPokemon,
-    indexOfLastPokemon
-  );
-  console.log(currentPokemons);
-  const numberOfPagesForFilteredPokemons = Math.ceil(
-    filteredPokemons.length / pokemonsPerPage
-  );
-
   return (
     <>
       <Box>
         <Grid container>
-          <form>
-            <input type="search" value={searchByName} onChange={handleChange} />
-          </form>
-          <Page tenPokemons={currentPokemons} />
+          <FilterablePokemon />
         </Grid>
       </Box>
-      <Pagination
-        numberOfPages={numberOfPagesForFilteredPokemons}
-        currentPage={currentPage}
-        setCurrentPage={setCurrentPage}
-        pageLimit={10}
-      />
     </>
   );
 };
