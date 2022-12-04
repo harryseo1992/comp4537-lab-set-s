8507b510a54d6e93bfe6037b6c42ebb2f60e03a8
Jong Hwan Seo <jseo51@my.bcit.ca>
2022-12-03 23:25:48 -0800

    Added created_at timestamp

--- Assignment3/part1/back-end/PokeLogSchema.js
+++ Assignment3/part1/back-end/PokeLogSchema.js
@@ -1,17 +1,25 @@
 const mongoose = require("mongoose");
 
-const schema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
+const schema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    endpoint: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: Number,
+    },
   },
-  endpoint: {
-    type: String,
-    required: true,
-  },
-  status: {
-    type: Number,
-  },
-});
+  {
+    timestamps: {
+      createdAt: "created_at", // Use `created_at` to store the created date
+      updatedAt: "updated_at", // and `updated_at` to store the last updated date
+    },
+  }
+);
 
 module.exports = mongoose.model("pokeLog", schema);
