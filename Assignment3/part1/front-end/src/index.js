003f1d9d046965f56f222eded36d2c78e0cabc06
Jong Hwan Seo <jseo51@my.bcit.ca>
2022-12-03 23:26:38 -0800

    Added separate path for pokedex client

--- Assignment3/part1/front-end/src/index.js
+++ Assignment3/part1/front-end/src/index.js
@@ -1,15 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./Components/AdminDashboard/Login";
+import FilterablePokemon from "./Components/FilterablePokemon/FilterablePokemon";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <h1>
       <Router>
-        <Login />
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/pokedex" element={<FilterablePokemon />} />
+        </Routes>
       </Router>
     </h1>
   </React.StrictMode>
