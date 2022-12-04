373fe93f5a5656f1ce6b67c927a2fdd1c733b294
Jong Hwan Seo <jseo51@my.bcit.ca>
2022-12-03 23:27:33 -0800

    Implemented report component

--- Assignment3/part1/front-end/src/Components/AdminDashboard/Report.js
+++ Assignment3/part1/front-end/src/Components/AdminDashboard/Report.js
@@ -1,28 +1,30 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import jwt_decode from "jwt-decode";
+import { BarChart } from "./BarChart";
+import { Chart as ChartJS } from "chart.js/auto";
+import { Chart } from "react-chartjs-2";
 
 const Report = ({ id, accessToken, setAccessToken, refreshToken }) => {
-  const [reportTable, setReportTable] = useState("");
-  useEffect(() => {
-    const start = async () => {
-      try {
-        const res = await axiosJWT.get(
-          `http://localhost:6969/report?id=${id}`,
-          {
-            headers: {
-              "auth-token-access": accessToken,
-            },
-          }
-        );
-        setReportTable(res.data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    start();
-  }, [id, accessToken]);
-
+  const [chartData, setChartData] = useState([]);
+  const [chartDataSet, setChartDataSet] = useState({
+    labels: "No data yet",
+    datasets: [
+      {
+        label: "Endpoint accessed",
+        data: null,
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+          "#ecf0f1",
+          "#50AF95",
+          "#f3ba2f",
+          "#2a71d0",
+        ],
+        borderColor: "black",
+        borderWidth: 2,
+      },
+    ],
+  });
   const refreshAccessToken = async () => {
     try {
       const res = await axios.post(
@@ -34,6 +36,7 @@ const Report = ({ id, accessToken, setAccessToken, refreshToken }) => {
           },
         }
       );
+      // setAccessToken(res.headers["auth-token-access"]);
       return res.headers["auth-token-access"];
     } catch (error) {
       console.log(error);
@@ -56,10 +59,128 @@ const Report = ({ id, accessToken, setAccessToken, refreshToken }) => {
     }
   );
 
+  useEffect(() => {
+    const start = async () => {
+      try {
+        const res = await axiosJWT.get(
+          `http://localhost:6969/report?id=${id}`,
+          {
+            headers: {
+              "auth-token-access": accessToken,
+            },
+          }
+        );
+        if (id == 1 && res.data) {
+          setChartDataSet({
+            labels: res.data.map((data) => data.date),
+            datasets: [
+              {
+                label: "Number of Users",
+                data: res.data.map((data) => data.count),
+                backgroundColor: [
+                  "rgba(75,192,192,1)",
+                  "#ecf0f1",
+                  "#50AF95",
+                  "#f3ba2f",
+                  "#2a71d0",
+                ],
+                borderColor: "black",
+                borderWidth: 2,
+              },
+            ],
+          });
+        }
+        if (id == 2 && res.data) {
+          setChartDataSet({
+            labels: res.data.map((data) => data._id[1].username),
+            datasets: [
+              {
+                label: "Number of Users",
+                data: res.data.map((data) => data.count),
+                backgroundColor: [
+                  "rgba(75,192,192,1)",
+                  "#ecf0f1",
+                  "#50AF95",
+                  "#f3ba2f",
+                  "#2a71d0",
+                ],
+                borderColor: "black",
+                borderWidth: 2,
+              },
+            ],
+          });
+        }
+        if (id == 3 && res.data) {
+          setChartDataSet({
+            labels: res.data.map((data) => data._id),
+            datasets: [
+              {
+                label: "Number of calls made by users",
+                data: res.data.map((data) => data.count),
+                backgroundColor: [
+                  "rgba(75,192,192,1)",
+                  "#ecf0f1",
+                  "#50AF95",
+                  "#f3ba2f",
+                  "#2a71d0",
+                ],
+                borderColor: "black",
+                borderWidth: 2,
+              },
+            ],
+          });
+        }
+        if (id == 4 && res.data) {
+          setChartDataSet({
+            labels: res.data.map((data) => data._id.endpoint),
+            datasets: [
+              {
+                label: "Number of calls made by users",
+                data: res.data.map((data) => data.count),
+                backgroundColor: [
+                  "rgba(75,192,192,1)",
+                  "#ecf0f1",
+                  "#50AF95",
+                  "#f3ba2f",
+                  "#2a71d0",
+                ],
+                borderColor: "black",
+                borderWidth: 2,
+              },
+            ],
+          });
+        }
+        if (id == 5 && res.data) {
+          setChartDataSet({
+            labels: res.data.map((data) => data._id.endpoint),
+            datasets: [
+              {
+                label: "Number of calls made by users",
+                data: res.data.map((data) => data.count),
+                backgroundColor: [
+                  "rgba(75,192,192,1)",
+                  "#ecf0f1",
+                  "#50AF95",
+                  "#f3ba2f",
+                  "#2a71d0",
+                ],
+                borderColor: "black",
+                borderWidth: 2,
+              },
+            ],
+          });
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    start();
+  }, [id, accessToken]);
+
   return (
     <>
       <div>Report {id && id}</div>
-      <div> {reportTable && reportTable}</div>
+      <BarChart chartDataSet={chartDataSet} />
     </>
   );
 };
