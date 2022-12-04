ce2700d1d2ee2d01e608987424bd3d957e384200
Jong Hwan Seo <jseo51@my.bcit.ca>
2022-12-03 23:27:55 -0800

    Added from lab 11

---
+++ Assignment3/part1/front-end/src/Components/FilterablePokemon/FilterablePokemon.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import FilteredPokemonPage from "../FiteredPokemonPage/FilteredPokemonPage";
+
+const FilterablePokemon = () => {
+  const [pokemons, setPokemons] = useState([]);
+  const [searchQueries, setSearchQueries] = useState({
+    searchByName: "",
+    searchByType: [],
+  });
+
+  useEffect(() => {
+    const fetchPokemons = () => {
+      axios
+        .get(
+          "https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/pokedex.json"
+        )
+        .then((res) => res.data)
+        .then((data) => setPokemons(data));
+    };
+    fetchPokemons();
+  }, []);
+
+  return (
+    <>
+      <FilteredPokemonPage
+        pokemons={pokemons}
+        searchQueries={searchQueries}
+        setSearchQueries={setSearchQueries}
+      />
+    </>
+  );
+};
+
+export default FilterablePokemon;
